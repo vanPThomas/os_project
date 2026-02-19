7f1dd1273f11033c67cabdbaea2fcd2998502d73
Thomas Van Petegem <thomas.van.petegem@hotmail.com>
2026-02-19 12:50:25 +0100

    setting up for wifi

--- src/main.cpp
+++ src/main.cpp
@@ -99,7 +99,7 @@ int main() {
     }
 
     Font::center_print(display, 1, "PICO OS");
-    Font::center_print(display, 3, "v0.1.5 - 2026");
+    Font::center_print(display, 3, "v0.1.6 - 2026");
     Font::center_print(display, 5, "PRESS ANY KEY");
 
     sleep_ms(2000);
