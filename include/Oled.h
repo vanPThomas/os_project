9f947e273f56fa40fa60433b7b2c358b36c2bca2
Thomas Van Petegem <thomas.van.petegem@hotmail.com>
2026-03-17 11:19:20 +0100

    Preparing final removal of the pico-sdk

--- include/Oled.h
+++ include/Oled.h
@@ -9,14 +9,13 @@
 class Oled
 {
 public:
-    Oled(i2c_inst_t* i2c_inst = i2c0,
-         uint sda_pin = 4,
-         uint scl_pin = 5,
-         uint speed_hz = 400000,
-         uint8_t addr = 0x3C,
-         uint16_t width = 128,
-         uint16_t height = 64,
-         uint16_t ram_width = 132);  // SH1106 needs 132 columns internally
+    Oled(uint32_t sda_pin = 4,
+     uint32_t scl_pin = 5,
+     uint32_t speed_hz = 400000,
+     uint8_t addr = 0x3C,
+     uint16_t width = 128,
+     uint16_t height = 64,
+     uint16_t ram_width = 132);  // SH1106 needs 132 columns internally
 
     ~Oled() = default;
 
@@ -40,9 +39,9 @@ public:
 
 private:
     i2c_inst_t* i2c_;
-    uint sda_pin_;
-    uint scl_pin_;
-    uint speed_hz_;
+    uint32_t sda_pin_;
+    uint32_t scl_pin_;
+    uint32_t speed_hz_;
     uint8_t addr_;
     uint16_t width_;       // visible width (usually 128)
     uint16_t height_;      // visible height (64)
